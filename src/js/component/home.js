e43bd338a69df60ab8b8132fd1a7793c9159fa8d
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-05-14 13:31:32 +0000

    creating the form of semaphore

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,23 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-export function Home() {
+function Home() {
+    function changeColor() {
+        
+        const [Color, setColor] = useState (0);
+        
+ }
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="trafficTop"></div>
+			<div className="container">
+				<button className="light firstColor" onClick= "changeColor">
+                               
+                </button>
+				<div className="light firstColor"></div>
+				<div className="light firstColor"></div>
+			</div>
 		</div>
 	);
 }
+export default Home;
