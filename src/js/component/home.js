5e3a524ca22951ec97a9b6634865afd97fa7ca59
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-05-31 17:23:23 +0000

    add review change colors

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,16 +2,25 @@ import React from "react";
 
 function Home() {
 	const [active, setActive] = React.useState("light inactive");
+	const [active1, setActive1] = React.useState("light inactive");
+	const [active2, setActive2] = React.useState("light inactive");
+
 	function changeRed() {
 		setActive("light red");
+		setActive1("light inactive");
+		setActive2("light inactive");
 	}
-	const [active1, setActive1] = React.useState("light inactive");
+
 	function changeYellow() {
 		setActive1("light yellow");
+		setActive("light inactive");
+		setActive2("light inactive");
 	}
-	const [active2, setActive2] = React.useState("light inactive");
+
 	function changeGreen() {
 		setActive2("light green");
+		setActive1("light inactive");
+		setActive("light inactive");
 	}
 
 	return (
